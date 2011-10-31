27eb9a1d98c6b3716fb541848b5238212f8e799c
Vilius Bekertas <bekertas@gmail.com>
2011-10-31 14:01:49 +0200

    me dead :?

---
+++ src/me/dalton/capturethepoints/Spawn.java
@@ -0,0 +1,20 @@
+package me.dalton.capturethepoints;
+
+ /** A Spawn Point in a CTP arena */
+public class Spawn {
+    /** X co-ordinate of this Spawn */
+    public double x;
+    
+    /** Y co-ordinate of this Spawn */
+    public double y;
+    
+    /** Z co-ordinate of this Spawn */
+    public double z;
+    
+    /** Direction players Spawn here */
+    public double dir = 0.0D;
+    
+    /** The name of this Spawn. (i.e. the team color name) */
+    public String name;
+    
+}
